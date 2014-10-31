4f8cb59f4dae29e5a2fd65e840f9959453785944
Davs <kovdavid@gmail.com>
2014-10-31 22:25:11 +0100

    Test code scaffolding

---
+++ spec/FCleaner_spec.rb
@@ -0,0 +1,7 @@
+require_relative 'spec_helper.rb'
+
+describe "test" do
+  it "should work" do
+    expect(5).to eq(5)
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,5 @@
-require 'unit_helper'
-
 RSpec.configure do |config|
-  config.mock_with :rspec do |mocks|
+  config.mock_with :rspec do |mock|
     mock.verify_doubled_constant_names = true
   end
 end
