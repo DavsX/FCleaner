f3ae8ce3e8a5baa1fa252cee7762c4d52585ecc2
Davs <kovdavid@gmail.com>
2014-11-03 11:11:37 +0100

    Add user_id and registration_year implementation

--- lib/FCleaner.rb
+++ lib/FCleaner.rb
@@ -1,5 +1,46 @@
-require "FCleaner/version"
+require_relative 'FCleaner/version'
+require 'mechanize'
 
 module FCleaner
-  # Your code goes here...
+  FACEBOOK_URL = 'https://m.facebook.com'
+  PROFILE_URL = "#{FACEBOOK_URL}/profile"
+  ACTIVITY_LOG_URL_TEXT = 'Activity Log'
+
+  def self.init(mail, pass)
+    @mail = mail
+    @pass = pass
+    @mech = Mechanize.new { |agent| agent.user_agent_alias = 'iPhone' }
+  end
+
+  def self.get_user_id()
+    @profile_page ||= @mech.get(PROFILE_URL)
+    link = @profile_page.links_with(:text => ACTIVITY_LOG_URL_TEXT).first
+    @user_id = link.href.gsub(/^#{FACEBOOK_URL}\//,'').gsub(/\/.*$/, '')
+  end
+
+  def self.get_registration_year()
+    page = @mech.get(activity_log_url)
+    year_links = page.parser.xpath("//div[@id[starts-with(.,'year_')]]")
+    reg_year = year_links.collect do |x|
+      x.attribute('id').to_s.gsub(/^year_/, '')
+    end.min
+
+    if reg_year
+      reg_year
+    else
+      Date.today.year
+    end
+  end
+
+  def self.activity_log_url()
+    "#{FACEBOOK_URL}/#{@user_id}/allactivity"
+  end
 end
+
+#search_form = page.form
+#search_form.field_with(:name => 'email').value = 'davserer@gmail.com'
+#search_form.field_with(:name => 'pass').value = 'mypassword'
+
+#new_page = agent.submit search_form
+
+#puts new_page
--- spec/FCleaner_spec.rb
+++ spec/FCleaner_spec.rb
@@ -1,7 +1,58 @@
-require_relative 'spec_helper.rb'
+require_relative 'spec_helper'
 
-describe "test" do
-  it "should work" do
-    expect(5).to eq(5)
+describe "FCleaner" do
+  before :each do
+    FCleaner.init('mail', 'pass')
+  end
+
+  it "saves auth data" do
+    expect(FCleaner.instance_variable_get :@mail).to eq('mail')
+    expect(FCleaner.instance_variable_get :@pass).to eq('pass')
+  end
+
+  it "creates mechanizer instance" do
+    mech = FCleaner.instance_variable_get :@mech
+
+    expect(mech).to be_an_instance_of(Mechanize)
+    expect(mech.user_agent).to match(/iPhone/)
+  end
+
+  it "knows profile url" do
+    expect(FCleaner::PROFILE_URL).to eq('https://m.facebook.com/profile')
+  end
+
+  it "gets user id" do
+    html = File.read('spec/mock_html/profile.html')
+    FakeWeb.register_uri( :get, FCleaner::PROFILE_URL,
+                          :body => html,
+                          :content_type => 'text/html')
+    expect(FCleaner.get_user_id()).to eq("100008460938593")
+  end
+
+  describe "getting registration year" do
+    before :each do
+      FCleaner.instance_variable_set(:@user_id, '123456')
+    end
+
+    it "builds activity log url" do
+      url = "https://m.facebook.com/123456/allactivity"
+      expect(FCleaner.activity_log_url).to eq(url)
+    end
+
+    it "succeeds when user is registered for more than a year" do
+      html = File.read('spec/mock_html/allactivity_reg_date.html')
+      FakeWeb.register_uri( :get, "https://m.facebook.com/123456/allactivity",
+                            :body => html,
+                            :content_type => 'text/html')
+      expect(FCleaner.get_registration_year()).to eq("2008")
+    end
+
+    it "succeeds when user is registered for more than a year" do
+      html = File.read('spec/mock_html/allactivity_reg_date_less_than_year.html')
+      FakeWeb.register_uri( :get, "https://m.facebook.com/123456/allactivity",
+                            :body => html,
+                            :content_type => 'text/html')
+      expect(FCleaner.get_registration_year()).to eq(Date.today.year)
+    end
   end
 end
